cd1849fa6c4b67c6d3cfffd1b606f912d564a4b1
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-08-25 19:51:04 -0300

    Update script.js

--- faq/script/script.js
+++ faq/script/script.js
@@ -1,17 +1,38 @@
-const mostrarmais = [...document.querySelectorAll(".mostrarmais")]
+const faq_question = [...document.querySelectorAll(".faq-question")]
 
-mostrarmais.map((el)=>{
+faq_question.map((el)=>{
     el.addEventListener("click",(evt)=>{
         let sibling = evt.target.previousElementSibling
         if(sibling == null){
             sibling = evt.target.nextElementSibling
+        } 
+        if (sibling.classList.contains("mostrarmais")){
+            if(sibling.firstElementChild.style.display != "block") {
+                sibling = sibling.firstElementChild
+            } else {
+                sibling = sibling.lastElementChild
+            }
         }
-        evt.target.style.display = "none"
+        if (evt.target.classList.contains("faq-question")){
+            sibling = evt.target.firstElementChild.nextElementSibling
+            if(sibling.firstElementChild.style.display != "block") {
+                sibling = sibling.firstElementChild
+            } else {
+                sibling = sibling.lastElementChild
+            }
+        }
+        let sibling2 = sibling.nextElementSibling
+        if(sibling2 == null){
+            sibling2 = sibling.previousElementSibling
+        }
+        sibling2.style.display = "none"
         sibling.style.display = "block"
-        if (sibling.classList.contains("more")){
-            evt.target.parentNode.parentNode.nextElementSibling.style.display = "block"
+        if (sibling2.classList.contains("more")){
+            //sibling.style.display = "block"
+            //console.log(sibling)
+            sibling.parentNode.parentNode.nextElementSibling.style.display = "none"
         } else {
-            evt.target.parentNode.parentNode.nextElementSibling.style.display = "none"
+            sibling2.parentNode.parentNode.nextElementSibling.style.display = "block"
         }
     })
 })
