98da68ed8b7bca8b91db7e450e8f031eda1ec13d
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-09-01 13:29:22 -0300

    home v5

---
+++ script/script.js
@@ -0,0 +1,45 @@
+const btn_prev = document.querySelector(".prev")
+const btn_next = document.querySelector(".next")
+const imagens = [
+    '../imagens/oncapintada.jpg',
+    '../imagens/arara.jpg',
+    '../imagens/jacare.jpg'
+]
+const img = document.querySelector(".main_imagem")
+const indicadores = [...document.querySelectorAll('.indicador')]
+let atual_index = 0
+function attIndicador(){
+    indicadores.map((el) => {
+        el.classList.remove('indicador_ativo')
+    })
+    indicadores[atual_index].classList.add('indicador_ativo')
+}
+function mostrarImg(index){
+        img.src = imagens[index]
+        attIndicador()
+}
+indicadores.map((el)=>{
+    el.addEventListener("click",(evt)=>{
+        atual_index = parseInt(evt.target.id)
+        mostrarImg(evt.target.id)
+        
+    })
+})
+btn_prev.addEventListener("click", ()=>{
+    atual_index = (atual_index === 0) ? imagens.length - 1 : atual_index - 1
+    mostrarImg(atual_index)
+    mudarTemp(10000)
+})
+function next(){
+    atual_index = (atual_index === imagens.length - 1) ? 0 : atual_index + 1
+    mostrarImg(atual_index)
+}
+function mudarTemp(time) {
+    clearInterval(img_temp)
+    img_temp = setInterval(next, time)
+}
+btn_next.addEventListener("click",()=>{
+    next()
+    mudarTemp(10000)
+})
+let img_temp = setInterval(next, 4000)
